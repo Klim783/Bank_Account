020da7326762323d893768cb8e3f6d764c6a63bf
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-14 11:13:58 +0300

    include routers for users

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.api.v1.wallets import router as wallet_router
 from app.api.v1.operations import router as operation_router
-
+from app.api.v1.users import router as users_router
 
 app = FastAPI()
 
 app.include_router(wallet_router, prefix = '/api/v1', tags = ['wallet'])
 app.include_router(operation_router, prefix = '/api/v1', tags = ['operations'])
-
+app.include_router(users_router, prefix='/api/v1', tags = ['users'])
 
 
 Base.metadata.create_all(bind=engine)
