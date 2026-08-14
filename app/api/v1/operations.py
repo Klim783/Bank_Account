478f52914079c7c2c0d14fe3963c959c1d3ae891
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-14 12:28:56 +0300

    finished functions for users

--- app/api/v1/operations.py
+++ app/api/v1/operations.py
@@ -1,17 +1,19 @@
-from fastapi import APIRouter
-from fastapi.params import Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.dependency import get_db
+from app.dependency import get_db, get_current_user
+from app.models import User
 from app.schemas import OperationRequest
 from app.services import operations as operations_services
 
 router = APIRouter()
 
 @router.post('/operations/income')
-def add_income(operation: OperationRequest, db: Session = Depends(get_db)):
-    return operations_services.add_income(operation)
+def add_income(operation: OperationRequest, db: Session = Depends(get_db),
+               current_user: User = Depends(get_current_user)):
+    return operations_services.add_income(db, current_user, operation)
 
 @router.post('/operations/expense')
-def add_expense (operation: OperationRequest, db: Session = Depends(get_db)):
-    return operations_services.add_expense(operation)
+def add_expense (operation: OperationRequest, db: Session = Depends(get_db),
+                 current_user: User = Depends(get_current_user)):
+    return operations_services.add_expense(db,current_user, operation)
--- app/api/v1/wallets.py
+++ app/api/v1/wallets.py
@@ -2,16 +2,21 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
+
+from app.api.v1.users import get_current_user
 from app.dependency import get_db
+from app.models import User
 from app.services import wallets as wallets_service
 from app.schemas import CreateWalletRequest
 
 router = APIRouter()
 
 @router.get('/balance')
-def get_balance(wallet_name:str|None = None, db: Session = Depends(get_db)):
-    return wallets_service.get_balance(wallet_name)
+def get_balance(wallet_name:str|None = None, db: Session = Depends(get_db),
+                current_user: User = Depends(get_current_user)):
+    return wallets_service.get_balance(db, current_user, wallet_name)
 
 @router.post('/wallets')
-def create_wallet(wallet:CreateWalletRequest, db: Session = Depends(get_db)):
-    return wallets_service.create_wallet(wallet)
+def create_wallet(wallet:CreateWalletRequest, db: Session = Depends(get_db),
+                  current_user: User = Depends(get_current_user)):
+    return wallets_service.create_wallet(db,current_user, wallet)
