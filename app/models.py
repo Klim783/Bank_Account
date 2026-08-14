71de9e9a68d0c0f0528b6193234f9c27e1f4ba54
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-14 10:14:29 +0300

    add new class - user

--- app/models.py
+++ app/models.py
@@ -1,10 +1,22 @@
+from sqlalchemy import ForeignKey
+
 from app.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from decimal import Decimal
+
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login : Mapped[int] = mapped_column(unique = True)
+
+
+
 class Wallet(Base):
     __tablename__ = 'wallet'
 
     id : Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     balance: Mapped[Decimal]
-
+    user_id : Mapped[int] = mapped_column(ForeignKey('user.id'), nullable = False)
