c92aaf1a36c012d89d2f5ac619dad29bc2282eda
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-15 00:08:09 +0300

    chaned function get_total_balance

--- app/services/wallets.py
+++ app/services/wallets.py
@@ -1,22 +1,24 @@
+from decimal import Decimal
+
 from fastapi import HTTPException
 
+from app.enum import CurrencyEnum
 from app.models import User
-from app.schemas import CreateWalletRequest, WalletResponse
+from app.schemas import CreateWalletRequest, WalletResponse, TotalBalance
 from app.repository import wallets as wallets_repository
 from sqlalchemy.orm import Session
+from app.services import exhange_service
 
-
-def get_balance(db: Session,current_user: User, wallet_name: str | None = None):
-    if wallet_name is None:
-        wallets = wallets_repository.get_all_wallets(db, current_user.id)
-        return {'total_balance': sum([w.balance for w in wallets])}
-    if not wallets_repository.is_wallet_exist(db, current_user.id ,wallet_name):
-        raise HTTPException(status_code=404, detail=f'Wallet {wallet_name} not found')
-    wallet = wallets_repository.get_wallet_balance_by_name(db, current_user.id, wallet_name)
-    db.commit()
-    return {'wallet': wallet.name, 'balance': wallet.balance}
-
-
+async def get_total_balance(db: Session,current_user: User) -> TotalBalance:
+    wallets = wallets_repository.get_all_wallets(db, current_user.id)
+    total_balance = Decimal(0)
+    for wallet in wallets:
+        if wallet.currency == CurrencyEnum.RUB:
+            total_balance += wallet.initial_balance
+        else:
+            exchange_rate = await exhange_service.get_exchange_rate(wallet.currency, CurrencyEnum.RUB)
+            total_balance += exchange_rate * wallet.balance
+        return TotalBalance(total_balance = total_balance)
 
 def create_wallet(db: Session, current_user: User, wallet : CreateWalletRequest) -> WalletResponse:
     if wallets_repository.is_wallet_exist(db, current_user.id, wallet.name):
