c228af277ac5d048f33f503034d7a07faf9a60e4
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-16 22:34:11 +0300

    add a field for password

--- app/repository/users.py
+++ app/repository/users.py
@@ -5,8 +5,8 @@ def get_user(db: Session, login: str) -> User | None:
     return db.query(User).filter(User.login == login).first()
 
 
-def create_user(db: Session, login: str) -> User:
-    user = User(login=login)  # 1. Передаем login
+def create_user(db: Session, login: str, hashed_password:str) -> User:
+    user = User(login=login, hashed_password = hashed_password)  # 1. Передаем login
     db.add(user)
     db.commit()               # 2. Фиксируем в БД
     db.refresh(user)          # 3. Обновляем объект (получаем id из БД)
