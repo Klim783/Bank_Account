bb96c12cf910a90c4e6827082a8a51a990e63cf0
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-16 22:36:47 +0300

    add a new router for login

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.dependency import get_db, get_current_user
 from app.models import User
-from app.schemas import UserRequest, UserResponse
+from app.schemas import UserRequest, UserResponse, TokenResponse, LoginRequest
 from app.services import users as users_service
 
 router = APIRouter()
@@ -11,6 +11,11 @@ router = APIRouter()
 @router.post('/users', response_model=UserResponse)
 def create_user(payload : UserRequest, db:Session = Depends(get_db)):
 	return users_service.create_user(db, payload.login)
+
+@router.post('/login', response_model=TokenResponse)
+def login(payload: LoginRequest, db: Session = Depends(get_db)):
+	return users_service.login(db, payload.login, payload.password)
+
 
 @router.get('/users', response_model=UserResponse)
 def get_current_user(current_user: User = Depends(get_current_user)):
