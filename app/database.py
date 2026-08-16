19c5bbab3873873997c01cee32eb5dcfd9aebb8d
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-16 11:06:52 +0300

    changed to postrgres settings

--- app/database.py
+++ app/database.py
@@ -1,10 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
 
-DATABASE_URL = 'sqlite:///./finance.db'
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 Base = declarative_base()
