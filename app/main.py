47cf116122f2f210445a68905a62054004a8c4d6
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-16 11:09:17 +0300

    remove create_all_tables for Alembic migration

--- app/main.py
+++ app/main.py
@@ -12,13 +12,10 @@ app.include_router(operation_router, prefix = '/api/v1', tags = ['operations'])
 app.include_router(users_router, prefix='/api/v1', tags = ['users'])
 
 app.mount('/static', StaticFiles(directory='app/static'), name = 'static')
-Base.metadata.create_all(bind=engine)
 
 from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # на проде сузить
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"]
 )
